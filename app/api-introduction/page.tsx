967b9bba7c7281aedfbf73469390f4c8046678ed
ztake8 <ztake8@gmail.com>
2025-11-17 03:36:01 +0530

    Fix link in sales section to point to correct URL

--- app/api-introduction/page.tsx
+++ app/api-introduction/page.tsx
@@ -27,7 +27,7 @@ export default function APIIntroductionPage() {
 
       <p>
         For sales - Submit a form here -{" "}
-        <a href="https://ztake.in/contact">https://pay.ztake.in/contact</a>
+        <a href="https://pay.ztake.in/contact">https://pay.ztake.in/contact</a>
       </p>
 
       <h2 id="easy-navigation">Easy navigation</h2>
